2bf802cf0b39a2f145d13cf86c79689945abb478
Koding Kidd <105122189+kodingfinesse@users.noreply.github.com>
2024-01-13 02:21:55 -0800

    01-13-2024

--- configuration.nix
+++ configuration.nix
@@ -1,58 +1,34 @@
 { config, pkgs, lib, ... }:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in
+
 {
   imports =
-    [ 
+    [
       ./hardware-configuration.nix
-      (import "${home-manager}/nixos")
     ];
 
-  # Enable Flakes and the new command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.system-features = [ "gccarch-znver3" ];
-  environment.sessionVariables = rec {
-    XDG_CACHE_HOME  = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME   = "$HOME/.local/share";
-    XDG_STATE_HOME  = "$HOME/.local/state";
-    NIX_CONF_DIR = "$HOME/.config/nix/nix.conf";
 
-    # Not officially in the specification
-    XDG_BIN_HOME    = "$HOME/.local/bin";
-    PATH = [ 
-      "${XDG_BIN_HOME}"
-    ];
+#  boot.loader.systemd-boot.enable = true;
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
+    grub = {
+       efiSupport = true;
+       device = "nodev";
+       useOSProber = true;
+    };
   };
-# Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-  
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-19.1.9"
-    "imagemagick-6.9.12-68"
-  ];
 
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-#  services.blueman.enable = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -67,24 +43,22 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.flatpak.enable = true;
+  # services
 
-  # Enable the KDE Plasma Desktop Environment.
+  services.flatpak.enable = true;
+  services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -93,36 +67,51 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
   };
+  programs.dconf.enable = true;
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+ # bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+ # zsh
+  programs.zsh = {
+    enable = true;
+    histSize = 0;
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+ # packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-19.1.9"
+    "imagemagick-6.9.12-68"
+  ];
+
   users.users.darren = {
     isNormalUser = true;
     description = "Darren Tran";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs.nur.repos; [
       vanilla.fastfetch
+      wolfangaukang.vdhcoapp
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-#  nixpkgs.config.allowInsecure = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-      amd-blis
-      amdgpu_top
       amd-libflame
       microcodeAmd
-      radeon-profile
-      nvtop-amd
       firefox
       thunderbird
       google-chrome
@@ -147,7 +136,6 @@ in
       fzf
       autojump
       cups
-      hplipWithPlugin
       freshfetch
       zsh
       sudo
@@ -166,50 +154,18 @@ in
       intel-one-mono
       league-spartan
       plank
+      nextcloud-client
   ];
 
-environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    gwenview
-    elisa
+  # excluded packages
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+     gwenview
+     elisa
   ];
 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.dconf.enable = true;
-  programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
-  programs.zsh = {
-    enable = true;
-    histSize = 0;
-  };
-  users.defaultUserShell = pkgs.zsh;
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
+  services.xserver.excludePackages = with pkgs; [
+     xterm
+  ];
 
   system.stateVersion = "23.11";
 }
