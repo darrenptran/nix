f804e979117dc53ccd1bf401f375e0088ca035f1
Koding Kidd <105122189+duke2sd@users.noreply.github.com>
2024-04-30 01:34:52 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,70 @@
-GNU nano 7.2                                       /etc/nixos/configuration.nix
+{ config, pkgs, libs, ... }:
+
+{
+imports =
+[
+./hardware-configuration.nix
+];
+
+# Bootloader.
+#  boot.loader.systemd-boot.enable = true;
+#  boot.loader.efi.canTouchEfiVariables = true;
+boot.loader = {
+efi = {
+canTouchEfiVariables = true;
+efiSysMountPoint = "/boot/efi";
+};
+grub = {
+efiSupport = true;
+device = "nodev";
+};
+};
+
+networking.hostName = "nixos"; # Define your hostname.
+networking.networkmanager.enable = true;
+time.timeZone = "America/Los_Angeles";
+
+i18n.defaultLocale = "en_US.UTF-8";
+i18n.extraLocaleSettings = {
+LC_ADDRESS = "en_US.UTF-8";
+LC_IDENTIFICATION = "en_US.UTF-8";
+LC_MEASUREMENT = "en_US.UTF-8";
+LC_MONETARY = "en_US.UTF-8";
+LC_NAME = "en_US.UTF-8";
+LC_NUMERIC = "en_US.UTF-8";
+LC_PAPER = "en_US.UTF-8";
+LC_TELEPHONE = "en_US.UTF-8";
+LC_TIME = "en_US.UTF-8";
+};
+
+# Enable the X11 windowing system.
+services.xserver.enable = true;
+
+# Enable the KDE Plasma Desktop Environment.
+services.xserver.displayManager.sddm.enable = true;
+services.xserver.desktopManager.plasma5.enable = true;
+
+# Configure keymap in X11
+services.xserver = {
+layout = "us";
+xkbVariant = "";
+};
+
+# Enable CUPS to print documents.
+services.printing.enable = true;
+
+# Enable sound with pipewire.
+sound.enable = true;
+hardware.pulseaudio.enable = false;
+security.rtkit.enable = true;
+services.pipewire = {
+enable = true;
+alsa.enable = true;
+alsa.support32Bit = true;
+pulse.enable = true;
+jack.enable = true;
+};
+
 programs.zsh = {
 enable = true;
 histSize = 0;
